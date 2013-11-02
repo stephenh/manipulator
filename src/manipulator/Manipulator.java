9ff3b473c021bde4c0692f6425399988d10f6f79
Stephen Haberman <stephen@exigencecorp.com>
2013-11-01 23:44:02 -0500

    Basically working.

--- src/manipulator/Manipulator.java
+++ src/manipulator/Manipulator.java
@@ -1,29 +1,35 @@
 package manipulator;
 
+import org.apache.commons.io.FileUtils;
+
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 import manipulator.types.EvaluatedType;
 
 public class Manipulator {
 
-	public static void main(String[] args) throws Exception {
-    // for (File victim : FileUtils.listFiles(new File("victims/input"), new String[] { "java" },
-    // true)) {
-    // System.err.printf("%s\n", victim.getCanonicalPath());
-    // EvaluatedType et = new EvaluatedType(victim);
-    // String boilerPlatedEdition = et.getJavadocBoilerPlatedEdition();
-    // System.out.println(boilerPlatedEdition);
-    // String outputPath = et.filePath.replace("victims/input", "victims/output");
-    // FileUtils.writeStringToFile(new File(outputPath), boilerPlatedEdition, false);
-    // }
-	  
-    File victim =
-        new File("/home/stephen/other/gwt/user/src/com/google/gwt/dom/client/IsElement.java");
-    EvaluatedType et = new EvaluatedType(victim);
-    String boilerPlatedEdition = et.getJavadocBoilerPlatedEdition();
-    System.out.println(boilerPlatedEdition);
-    // FileUtils.writeStringToFile(victim, boilerPlatedEdition);
-	}
-
+  public static void main(String[] args) throws Exception {
+    File base = new File("/home/stephen/other/gwt/user/src");
+    List<File> scan = new ArrayList<>();
+    scan.add(new File(base, "/com/google/gwt/dom/client"));
+    scan.add(new File(base, "/com/google/gwt/user/client/ui"));
+    scan.add(new File(base, "/com/google/gwt/user/datepicker/client"));
+    for (File dir : scan) {
+      for (File victim : FileUtils.listFiles(dir, new String[] { "java" }, false)) {
+        if (victim.getName().contains("IsWidget") || victim.getName().contains("IsRenderable") || victim.getName().contains("IsTreeItem")) {
+          continue;
+        }
+        if (!victim.getName().startsWith("Is")) {
+          continue;
+        }
+        System.out.println("Updating " + victim.getName());
+        EvaluatedType et = new EvaluatedType(victim);
+        String boilerPlatedEdition = et.getJavadocBoilerPlatedEdition();
+        FileUtils.writeStringToFile(victim, boilerPlatedEdition);
+      }
+    }
+  }
 
 }
--- src/manipulator/types/EvaluatedType.java
+++ src/manipulator/types/EvaluatedType.java
@@ -2,13 +2,15 @@ package manipulator.types;
 
 import org.antlr.v4.runtime.ANTLRFileStream;
 import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.Token;
 import org.antlr.v4.runtime.TokenStreamRewriter;
 import org.antlr.v4.runtime.tree.ParseTreeWalker;
 import org.apache.commons.lang3.StringUtils;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Map;
+import java.util.List;
 import java.util.TreeMap;
 
 import manipulator.antlr.JavaBaseListener;
@@ -16,120 +18,172 @@ import manipulator.antlr.JavaLexer;
 import manipulator.antlr.JavaParser;
 import manipulator.antlr.JavaParser.FormalParameterContext;
 import manipulator.antlr.JavaParser.FormalParametersContext;
+import manipulator.antlr.JavaParser.InterfaceDeclarationContext;
 import manipulator.antlr.JavaParser.InterfaceMethodDeclarationContext;
 
 public class EvaluatedType {
-	public final String filePath;
-	private CommonTokenStream tokenStream = null;
-	public TreeMap<String, InterfaceMethodDeclarationContext> interfaceMethodContexts = new TreeMap<>();
-	public String packageName = null;
-	public String canonicalName = null;
-	public String simpleName = null;
-	public TreeMap<String, String> importedSimpleTypeNames = new TreeMap<>();
 
-	public EvaluatedType(File typeFile) {
-		try {
-			this.filePath = typeFile.getCanonicalPath();
-		} catch (Exception e) {
-			throw new RuntimeException(e);
-		}
-		this.parseTypeFile();
-	}
+  private final List<InterfaceMethodDeclarationContext> interfaceMethodContexts = new ArrayList<>();
+  private final TreeMap<String, String> importedSimpleTypeNames = new TreeMap<>();
+  private final CommonTokenStream tokenStream;
 
-	private void parseTypeFile() {
-		try {
-			JavaLexer lexer = new JavaLexer(new ANTLRFileStream(this.filePath));
-			this.tokenStream = new CommonTokenStream(lexer);
-			JavaParser parser = new JavaParser(this.tokenStream);
-			new ParseTreeWalker().walk(new JavaBaseListener() {
-				@Override
-				public void exitPackageDeclaration(JavaParser.PackageDeclarationContext ctx) {
-					EvaluatedType.this.packageName = ctx.qualifiedName().getText();
-				}
+  private String packageName = null;
+  private String canonicalName = null;
+  private String simpleName = null;
+  private String concreteName = null;
+  private InterfaceDeclarationContext intfDeclaration = null;
 
-				@Override
-				public void exitImportDeclaration(JavaParser.ImportDeclarationContext ctx) {
-					EvaluatedType.this.importedSimpleTypeNames.put(ctx.qualifiedName().stop.getText(), ctx.qualifiedName().getText());
-				}
+  public EvaluatedType(File typeFile) {
+    try {
+      JavaLexer lexer = new JavaLexer(new ANTLRFileStream(typeFile.getCanonicalPath()));
+      this.tokenStream = new CommonTokenStream(lexer);
+      this.parseTypeFile();
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+  }
 
-				@Override
-				public void enterInterfaceDeclaration(JavaParser.InterfaceDeclarationContext ctx) {
-					// Note that this bound *enter* rather than *exit* so that the canonical type name is immediately available for other purposes
-					EvaluatedType.this.simpleName = ctx.Identifier().getText();
-					EvaluatedType.this.canonicalName = EvaluatedType.this.packageName + "." + EvaluatedType.this.simpleName;
-				}
+  private void parseTypeFile() {
+    new ParseTreeWalker().walk(new JavaBaseListener() {
+      @Override
+      public void exitPackageDeclaration(JavaParser.PackageDeclarationContext ctx) {
+        packageName = ctx.qualifiedName().getText();
+      }
 
-				@Override
-				public void exitInterfaceMethodDeclaration(JavaParser.InterfaceMethodDeclarationContext ctx) {
-					String methodName = ctx.Identifier().getText();
-					EvaluatedType.this.interfaceMethodContexts.put(EvaluatedType.this.generateFriendlyMethodSignature(methodName, ctx.formalParameters()), ctx);
-				}
-			}, parser.compilationUnit());
-		} catch (Exception e) {
-			throw new RuntimeException(e);
-		}
-	}
+      @Override
+      public void exitImportDeclaration(JavaParser.ImportDeclarationContext ctx) {
+        importedSimpleTypeNames.put(ctx.qualifiedName().stop.getText(), ctx.qualifiedName().getText());
+      }
 
-	public String getJavadocBoilerPlatedEdition() {
-		TokenStreamRewriter rewriter = new TokenStreamRewriter(this.tokenStream);
-		for (Map.Entry<String, InterfaceMethodDeclarationContext> interfaceMethod : this.interfaceMethodContexts.entrySet()) {
-			InterfaceMethodDeclarationContext imdc = interfaceMethod.getValue();
-			String friendlyMethodDescriptor = interfaceMethod.getKey();
-      String concreteName = simpleName.replaceAll("^Is", "");
-			StringBuilder javadoc = new StringBuilder();
-			javadoc.append("/**\n");
-      javadoc.append("   * See {@link " + concreteName + "#" + friendlyMethodDescriptor + "}.\n");
-      javadoc.append("   */\n  ");
-			rewriter.insertBefore(imdc.parent.parent.getSourceInterval().a, javadoc.toString());
-		}
-		return rewriter.getText();
-	}
+      @Override
+      public void enterInterfaceDeclaration(JavaParser.InterfaceDeclarationContext ctx) {
+        // Note that this bound *enter* rather than *exit* so that the canonical type name is immediately available for other purposes
+        simpleName = ctx.Identifier().getText();
+        concreteName = simpleName.replaceAll("^Is", "");
+        canonicalName = packageName + "." + simpleName;
+        intfDeclaration = ctx;
+      }
 
-	public String resolveType(String potentiallyAlreadyCanonicalTypeName) {
-		// Is it already qualified?
-		if (potentiallyAlreadyCanonicalTypeName.contains(".")) {
-			return potentiallyAlreadyCanonicalTypeName; // presume so if there is a dot in the name
-		}
+      @Override
+      public void exitInterfaceMethodDeclaration(JavaParser.InterfaceMethodDeclarationContext ctx) {
+        interfaceMethodContexts.add(ctx);
+      }
+    }, new JavaParser(this.tokenStream).compilationUnit());
+  }
 
-		String[] arrayTypeSplit = potentiallyAlreadyCanonicalTypeName.split("\\[\\]");
-		boolean isArrayType = potentiallyAlreadyCanonicalTypeName.contains("[]");
-		String nonArraySimpleName = arrayTypeSplit[0];
+  public String getJavadocBoilerPlatedEdition() {
+    TokenStreamRewriter rewriter = new TokenStreamRewriter(tokenStream);
+    deleteExistingClassComment(rewriter);
+    addNewClassComment(rewriter);
+    for (InterfaceMethodDeclarationContext imdc : interfaceMethodContexts) {
+      deleteExistingMethodComment(rewriter, imdc);
+      if (imdc.Identifier().getText().equals("as" + concreteName)) {
+        addAsMethodComment(rewriter, imdc);
+      } else {
+        addNewMethodComment(rewriter, imdc);
+      }
+    }
+    return rewriter.getText();
+  }
 
-		// Is this an imported type?
-		if (this.importedSimpleTypeNames.containsKey(nonArraySimpleName)) {
-			return this.importedSimpleTypeNames.get(nonArraySimpleName) + (isArrayType ? "[]" : "");
-		}
+  private String resolveType(String potentiallyAlreadyCanonicalTypeName) {
+    // Is it already qualified?
+    if (potentiallyAlreadyCanonicalTypeName.contains(".")) {
+      return potentiallyAlreadyCanonicalTypeName; // presume so if there is a dot in the name
+    }
 
-		// Is it a self-referential type?
-		if (nonArraySimpleName.equals(this.simpleName)) {
-			return this.canonicalName + (isArrayType ? "[]" : "");
-		}
+    String[] arrayTypeSplit = potentiallyAlreadyCanonicalTypeName.split("\\[\\]");
+    boolean isArrayType = potentiallyAlreadyCanonicalTypeName.contains("[]");
+    String nonArraySimpleName = arrayTypeSplit[0];
 
-		// Is it on the search path?
-		try {
-			String searchPackageTypeName = "java.lang." + nonArraySimpleName;
-			Class.forName(searchPackageTypeName);
-			return searchPackageTypeName + (isArrayType ? "[]" : "");
-		} catch (Exception e) {
-			// Nope.
-		}
+    // Is this an imported type?
+    if (importedSimpleTypeNames.containsKey(nonArraySimpleName)) {
+      return importedSimpleTypeNames.get(nonArraySimpleName) + (isArrayType ? "[]" : "");
+    }
 
-		// Give up and just hand back the original value.
-		return potentiallyAlreadyCanonicalTypeName;
-	}
+    // Is it a self-referential type?
+    if (nonArraySimpleName.equals(simpleName)) {
+      return canonicalName + (isArrayType ? "[]" : "");
+    }
 
-	public String generateFriendlyMethodSignature(String methodName, FormalParametersContext fpc) {
-		ArrayList<String> types = new ArrayList<>();
-		StringBuilder typeSignature = new StringBuilder();
-		if (fpc.formalParameterList() != null) {
-			for (FormalParameterContext parameter : fpc.formalParameterList().formalParameter()) {
-				types.add(this.resolveType(parameter.type().getText()));
-			}
-			if (fpc.formalParameterList().lastFormalParameter() != null) {
-				types.add(this.resolveType(fpc.formalParameterList().lastFormalParameter().type().getText()) + "...");
-			}
-			typeSignature.append(StringUtils.join(types, ", "));
-		}
-		return String.format("%s(%s)", methodName, typeSignature.toString());
-	}
+    // Is it on the search path?
+    try {
+      String searchPackageTypeName = "java.lang." + nonArraySimpleName;
+      Class.forName(searchPackageTypeName);
+      return searchPackageTypeName + (isArrayType ? "[]" : "");
+    } catch (Exception e) {
+      // Nope.
+    }
+
+    // Give up and just hand back the original value.
+    return potentiallyAlreadyCanonicalTypeName;
+  }
+
+  private void addNewClassComment(TokenStreamRewriter rewriter) {
+    StringBuilder javadoc = new StringBuilder();
+    javadoc.append("/**\n");
+    javadoc.append(" * An interface for {@link " + concreteName + "}.\n");
+    javadoc.append(" */\n");
+    rewriter.insertBefore(intfDeclaration.parent.getSourceInterval().a, javadoc.toString());
+  }
+
+  private void addNewMethodComment(TokenStreamRewriter rewriter, InterfaceMethodDeclarationContext imdc) {
+    String methodName = imdc.Identifier().getText();
+    String signature = generateFriendlyMethodSignature(methodName, imdc.formalParameters());
+    StringBuilder javadoc = new StringBuilder();
+    javadoc.append("/**\n");
+    javadoc.append("   * See {@link " + concreteName + "#" + signature + "}.\n");
+    javadoc.append("   */\n  ");
+    rewriter.insertBefore(imdc.parent.parent.getSourceInterval().a, javadoc.toString());
+  }
+
+  private void addAsMethodComment(TokenStreamRewriter rewriter, InterfaceMethodDeclarationContext imdc) {
+    StringBuilder javadoc = new StringBuilder();
+    javadoc.append("/**\n");
+    javadoc.append("   * @return the concrete {@link " + concreteName + "}\n");
+    javadoc.append("   */\n  ");
+    rewriter.insertBefore(imdc.parent.parent.getSourceInterval().a, javadoc.toString());
+  }
+
+  private void deleteExistingMethodComment(TokenStreamRewriter rewriter, InterfaceMethodDeclarationContext imdc) {
+    // imdc InterfaceMethodDeclarationContext -> interfaceMemberDecl -> interfaceBodyDecl
+    List<Token> hidden = tokenStream.getHiddenTokensToLeft(imdc.getParent().getParent().start.getTokenIndex());
+    deleteAfterFirstComment(rewriter, hidden);
+  }
+
+  private void deleteExistingClassComment(TokenStreamRewriter rewriter) {
+    // ctx interfaceDeclaration -> parent typeDeclaration
+    List<Token> hidden = tokenStream.getHiddenTokensToLeft(intfDeclaration.getParent().start.getTokenIndex());
+    deleteAfterFirstComment(rewriter, hidden);
+  }
+
+  private void deleteAfterFirstComment(TokenStreamRewriter rewriter, List<Token> hidden) {
+    if (hidden != null && !hidden.isEmpty()) {
+      // could have: \n\n, /** blah */, \n -- and we want to keep the first \n\n
+      boolean foundComment = false;
+      for (Token token : hidden) {
+        if (token.getText().contains("/")) {
+          foundComment = true;
+        }
+        if (foundComment) {
+          rewriter.delete(token);
+        }
+      }
+    }
+  }
+
+  private String generateFriendlyMethodSignature(String methodName, FormalParametersContext fpc) {
+    ArrayList<String> types = new ArrayList<>();
+    StringBuilder typeSignature = new StringBuilder();
+    if (fpc.formalParameterList() != null) {
+      for (FormalParameterContext parameter : fpc.formalParameterList().formalParameter()) {
+        types.add(this.resolveType(parameter.type().getText()));
+      }
+      if (fpc.formalParameterList().lastFormalParameter() != null) {
+        types.add(this.resolveType(fpc.formalParameterList().lastFormalParameter().type().getText()) + "...");
+      }
+      typeSignature.append(StringUtils.join(types, ", "));
+    }
+    return String.format("%s(%s)", methodName, typeSignature.toString());
+  }
 }
